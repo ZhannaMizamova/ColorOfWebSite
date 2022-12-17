fa4c38d1d4bc9d1f3505905ef65ddf2e7f456a3a
ZhannaMizamova <kubeev2012@list.ru>
2022-12-18 01:14:10 +0600

    commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function show(color){
+     if (document.getElementById("siteColor").value == "green"){
+        document.body.style.backgroundColor="green";
+    }
+    else if (document.getElementById("siteColor").value == "white"){
+        document.body.style.backgroundColor="white";
+    }
+    else if (document.getElementById("siteColor").value == "yellow"){
+        document.body.style.backgroundColor="yellow";
+    }
+}
+
